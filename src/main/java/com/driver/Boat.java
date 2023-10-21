e48f5ead3379de8fd0372ab2563e09591774ad4d
Akash-more1 <akashmore9725@gmail.com>
2023-10-21 10:58:04 +0530

    second commit

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,5 +1,19 @@
 package com.driver;
 
-public class Boat {
+public class Boat extends Car implements WaterVehicle {
 
+
+    public Boat(String name,  int gears, boolean isManual, int seats) {
+        super(name, 0, 2, gears, isManual, "Boat", seats);
+    }
+
+    @Override
+    public String getVehicleName() {
+        return this.getName();
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return this.getSeats();
+    }
 }
